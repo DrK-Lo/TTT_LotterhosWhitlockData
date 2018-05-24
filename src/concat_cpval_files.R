1f4678dbd22dedd7d3a82fec34d43591b5ff00a5
bmford11 <brett.ford@ubc.ca>
2018-05-24 12:53:51 -0400

    Create concat_cpval_files.R
    This script takes the original Lotterhos-Whitlock Cpval files and appends the data for the dbrda and rda analyses

---
+++ src/concat_cpval_files.R
@@ -0,0 +1,51 @@
+######################################
+#
+# concat_cpval_files.R
+# Brett Ford
+# Created 20180314
+#
+# This script takes the original Lotterhos-Whitlock Cpval files
+# and appends the data for the dbrda and rda analyses
+#
+# This assumes that ordination files have SNPnames
+#
+# Usage: Rscript concat_cpval_files.R
+#
+######################################
+
+#Set directories
+base_path <- "/Users/brettford/Desktop/Northeastern/coding/forester_simulation_code/dryad"
+out_path <- "/Users/brettford/Desktop/Northeastern/coding/forester_simulation_code/forester_sim_code/output/"
+
+#Load libraries
+library(plyr)
+
+#Unlist ordination files output from running Brenna's script
+Filenames.ORD <- list.files(path=paste0(out_path,"/ORD_output/"), pattern="ORD_locus_stats") 
+
+for (i in 1:length(Filenames.ORD)) {               
+  sim1 <- read.table(paste0(out_path, "/ORD_output/", Filenames.ORD[[i]]), header = T, sep = " ")
+  sim1 <- sim1[,2:(ncol(sim1)-1)]
+
+  #Column names should include: method_software&version_stat_logical(if applicable)
+  colnames(sim1) <- c("SNPnames", "rda_veganv2.4-3_loading", "rda_veganv2.4-3_empP", "rda_veganv2.4-3_empQ", "rda_veganv2.4-3_empP_log", "rda_veganv2.4-3_SD2.5_log", 
+                               "rda_veganv2.4-3_SD3.0_log", "dbrda_veganv2.4-3_loading", "dbrda_veganv2.4-3_empP", "dbrda_veganv2.4-3_empQ", "dbrda_veganv2.4-3_empP_log",
+                               "dbrda_veganv2.4-3_SD2.5_log", "dbrda_veganv2.4-3_SD3.0_log")
+  
+  #Get basename of Cpval file from ordination files
+  cpval_file <- gsub(Filenames.ORD[[i]], pattern="ORD_locus_stats_", replacement="")
+  cpval_file <- gsub(cpval_file, pattern=".txt$", replacement="")
+  
+  #Read in Lotterhos Whitlock Cpval file
+  Cpvaltable <- read.table(paste0(base_path, "/SummaryFiles/", cpval_file, "Bayenv2LFMMpca.Cpval"), header = T, sep = " ")
+
+  # Create a lookup table from ordination file
+  lookup <- unique(sim1)
+  # Use join to append the lookup values to the Cpvaltable, based on SNPnames
+  plyr1 <- join(Cpvaltable, lookup, by = "SNPnames")
+
+  #write table to file (should write a total of 72 files)
+  write.table(plyr1, file = paste0(out_path, "concatenated_files/", cpval_file, "Bayenv2LFMMpca.Cpval"), sep = " ", row.names = F)
+}
+
+
